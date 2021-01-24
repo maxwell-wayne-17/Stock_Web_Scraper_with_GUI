05f4b0b92c26eb76ec0b215087341fb804169f0e
Max Wayne <maxwell.wayne.17@cnu.edu>
2021-01-24 14:24:30 -0500

    Moved scraping to definition.  Cleaned and commented code

--- GUItest.py
+++ GUItest.py
@@ -2,26 +2,49 @@ import PySimpleGUI as sg
 import requests
 from bs4 import BeautifulSoup
 
+# Function to scrape yahoo finance and get stock price from a given symbol
+def getStockPrice(symbol):
+    url = f'https://finance.yahoo.com/quote/{symbol}?p={symbol}'
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    price = soup.find(class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').text
+
+    return price
+
+
+# Create GUI Layout
 layout = [
+    # Message above text box
     [sg.Text('Please enter a stock symbol')],
+
+    # Create text box for user
     [
         sg.Text("Stock Symbol"),
         sg.In(size=(15, 1), enable_events=True, key="-SYMBOL-")
     ],
+
+    # Create area to display stock symbols and prices
     [
         sg.Listbox(
-            values=[], enable_events=True, size=(40,20), key="-STOCKS LIST-"
+            values=[], enable_events=True, size=(40, 20), key="-STOCKS LIST-"
         )
     ],
+
+    # Submit and cancel buttons
     [sg.Submit(), sg.Cancel()]
 ]
 
+# Open the labeled window with layout above
 window = sg.Window('Yahoo Stock Scraper', layout)
 
 stockSymbols = []
 stockPrices = []
+# Array to hold stock symbol and prices
+displayLines = []
 # Used to help format text
 emptyStr = ""
+
 while True:
 
     event, values = window.read()
@@ -30,7 +53,7 @@ while True:
     if event == "Cancel" or event == sg.WIN_CLOSED:
         break
 
-    # Event was enter in stock symbol text box or submit
+    # Event was enter in stock symbol text box
     if event == "-SYMBOL-":
         stock = values["-SYMBOL-"]
 
@@ -39,19 +62,16 @@ while True:
         # Add current text in box to array
         stockSymbols.append([stock])
 
-        # Search of yahoo finance page of inputted stock and get raw HTML (Split into definition later)
-        URL = f'https://finance.yahoo.com/quote/{stock}?p={stock}'
-        page = requests.get(URL)
-
-        # Parse HTML
-        soup = BeautifulSoup(page.content, 'html.parser')
-        results = soup.find(class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').text
+        # Get stock price
+        results = getStockPrice(stock)
         stockPrices.append([results])
 
-        # Wonky way to only add first in array, need to use for loop to update with entire list
+        # Add symbol and price to the display array
         lastIndex = len(stockSymbols) - 1
         line = f"{emptyStr.join(stockSymbols[lastIndex])} ${emptyStr.join(stockPrices[lastIndex])}"
-        window["-STOCKS LIST-"].update([line])
+        displayLines.append(line)
+
+        # Display the array
+        window["-STOCKS LIST-"].update(displayLines)
 
 window.close()
-
--- StockScraper.py
+++ StockScraper.py
@@ -4,19 +4,25 @@ from pip._vendor.distlib.compat import raw_input
 import PySimpleGUI as sg
 import os.path
 
+
+def getStockPrice(symbol):
+    url = f'https://finance.yahoo.com/quote/{symbol}?p={symbol}'
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    price = soup.find(class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').text
+
+    return price
+
+
 ongoing = True
 
 while ongoing:
 
     stockSymbol = raw_input("\nWhat stock value would you like to see?\n")
-    URL = 'https://finance.yahoo.com/quote/' + stockSymbol + '?p=' + stockSymbol
-    page = requests.get(URL)
-
-    soup = BeautifulSoup(page.content, 'html.parser')
-    results = soup.find(class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').text
+    results = getStockPrice(stockSymbol)
     print(results + '\n')
 
     check = raw_input("Would you like to look up another price? (yes/no)\n")
     if check.lower() == 'no':
         ongoing = False
-
